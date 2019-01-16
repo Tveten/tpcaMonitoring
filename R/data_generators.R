69f5004b9378cdd82f3d8622afb04c8587c1b077
Martin <matveten@gmail.com>
2019-01-16 19:25:50 +0100

    Updated function to conform with notation elsewhere. Also, different arguments since cov_mat from training is needed.

--- R/data_generators.R
+++ R/data_generators.R
@@ -1,45 +1,48 @@
+generate_cor_data <- function(d, n, mu0, Sigma0,
+                              kappa = 0, p = 0,
+                              mu = 0, sigma = 1, rho_scale = 1) {
+
+  change_mu <- function(mu0, mu, D) {
+    if (mu != 0) {
+      mu1[D] <- mu
+      return(mu1)
+    } else return(mu0)
   }
-  return(X)
-}
 
-generate_cor_data <- function(N, stream.length, K0 = N,
-                              K = 0, kappa = 0,
-                              mu = 0, sigma = 1, rho.scale = 1,
-                              return.cor.mat = FALSE) {
-
-  mu0 <- rep(0, N)
-  Sigma0 <- generate_cor_mat(N, K0 = K0)
-
-  if (K == 0)
-    X <- t(mvrnorm(stream.length, mu = mu0, Sigma = Sigma0))
-  else if (K > 0 && K <= N) {
-    affected.dims <- sample(1:N, K)
-
-    mu1 <- rep(0, N)
-    Sigma1 <- Sigma0
-    if (mu != 0) mu1[affected.dims] <- mu
-
-    if (sigma != 1 || rho.scale != 1) {
+  change_Sigma <- function(Sigma0, sigma, rho_scale, D) {
+    if (sigma != 1 || rho_scale != 1) {
       draw_sigma <- NULL
       draw_rho <- NULL
       if (sigma != 1) draw_sigma <- function(n) rep(sigma, n)
-      if (rho.scale != 1) draw_rho <- function(n) rep(rho.scale, n)
-      Sigma1 <- change_cor_mat(Sigma0, affected.dims, draw_rho, draw_sigma)
-    }
+      if (rho_scale != 1) draw_rho <- function(n) rep(rho_scale, n)
+      Sigma1 <- tpca::change_cor_mat(Sigma0, D,
+                                     draw_cor = draw_rho,
+                                     draw_sd  = draw_sigma)
+      return(Sigma1)
+    } else return(Sigma0)
+
+  }
+
+  mu0 <- rep(0, N)
+  k <- round(p * d)
+
+  if (k == 0)
+    x <- gen_norm_data(n, mu0, Sigma0)
+  else if (k > 0 && k <= d) {
+    affected_dims <- sample(1:d, k)
+    mu1 <- change_mu(mu0, mu, affected_dims)
+    Sigma1 <- change_Sigma(Sigma0, sigma, rho_scale, affected_dims)
 
     if (kappa == 0)
-      X <- t(mvrnorm(stream.length, mu = mu1, Sigma = Sigma1))
+      x <- gen_norm_data(n, mu1, Sigma1)
     else {
-      X0 <- t(mvrnorm(kappa, mu = mu0, Sigma = Sigma0))
-      X1 <- t(mvrnorm(stream.length - kappa, mu = mu1, Sigma = Sigma1))
-      X <- cbind(X0, X1)
+      x0 <- gen_norm_data(kappa, mu0, Sigma0)
+      x1 <- gen_norm_data(n - kappa, mu1, Sigma1)
+      x <- cbind(x0, x1)
     }
-  } else stop('Invalid number of affected streams K. No data generated.')
+  } else stop('Invalid proportion of affected streams p. No data generated.')
 
-  if (return.cor.mat)
-    return(list('X' = X, 'cor.mat' = Sigma0))
-  else
-    return(X)
+  return(x)
 }
 
 gen_norm_data <- function(n, mu, Sigma) {
