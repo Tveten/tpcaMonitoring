ce7600ec9e6be45bdb0e356db4bed86c850ecebc
Martin <matveten@gmail.com>
2019-05-20 16:12:47 +0200

    Code cleanup.

--- R/scree_plot.R
+++ R/scree_plot.R
@@ -2,11 +2,9 @@ scree_plots <- function() {
   n_sets <- 30
   d <- 100
   m <- 200
-  # training_sets <- get_training_sets(n_sets, d = d, m = m)
   eigen_values <- lapply(training_sets, function(train_obj) {
     x <- train_obj$x
     Sigma_hat <- 1 / (ncol(x) - 1) * (x %*% t(x))
-    # Sigma_hat <- train_obj$Sigma
     tpca::pca(Sigma_hat)$value
   })
   eigen_values <- do.call('cbind', eigen_values)
@@ -18,13 +16,10 @@ scree_plots <- function() {
   eigen_values_I <- do.call('cbind', eigen_values_I)
   eigen_values <- cbind(eigen_values, eigen_values_I)
 
-  # col <- c(colorRampPalette(c('red', 'darkgreen'))(n_sets), 'black')
   col <- c(rep('red', 15), rep('darkgreen', 15), rep('black', 15))
   legend_breaks <- c(1, 16, n_sets + 1)
   legend_labels <- c('High correlation', 'Low correlation', 'No correlation')
-  # line_sizes <- c(rep(0.2, n_sets), 0.5)
   line_sizes <- c(rep(0.2, n_sets + 15))
-  # title_str <- latex2exp::TeX("Scree plots of $\\hat{\\Sigma}_0$\'s")
   eigen_value_df <- reshape2::melt(eigen_values,
                                    varnames   = c('component_nr', 'cov_mat_nr'),
                                    value.name = 'eigen_value')
@@ -35,32 +30,23 @@ scree_plots <- function() {
     ggplot2::geom_line(ggplot2::aes(size = cov_mat_nr)) +
     ggplot2::theme_light() +
     ggplot2::labs(x = 'Component nr.', y = 'Eigen value') +
-    # ggplot2::coord_cartesian(ylim = ylim) +
     ggplot2::scale_color_manual('',
                                 values = col,
                                 breaks = legend_breaks,
                                 labels = legend_labels) +
     ggplot2::scale_size_manual(values = line_sizes, guide = FALSE)
-    # ggplot2::scale_linetype_manual(values = rep(1, length(unique(edd_df$method_param))), guide = FALSE)
-#
-#   for (i in seq_along(eigen_objs)) {
-#     ggplot2::qplot()
-#     Sys.sleep(1)
-#   }
 }
 
 cor_density <- function() {
   n_sets <- 30
   d <- 100
   m <- 200
-  # training_sets <- get_training_sets(n_sets, d = d, m = m)
   cors <- lapply(training_sets, function(train_obj) {
     x <- train_obj$x
     Sigma_hat <- 1 / (ncol(x) - 1) * (x %*% t(x))
     sd_x <- rowSds(x)
     Sigma_hat <- diag(1 / sd_x) %*% Sigma_hat %*% diag(1 / sd_x)
     Sigma_hat[lower.tri(Sigma_hat)]
-    # train_obj$Sigma[lower.tri(train_obj$Sigma)]
   })
   cors <- do.call('cbind', cors)
 
